05e3c8fb6dc7680146799280adcc93b3bdd252a6
Pavan Pandey <pandeypavan03@gmail.com>
2021-09-03 22:55:08 +0530

    second Commit

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  gotToDashboard(){
+    // alert("Hey I am dashboard");
+    //validate the user with api 
+    this.router.navigate(["/dashboard"])
+
   }
 
 }
